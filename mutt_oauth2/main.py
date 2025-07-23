a955fb39d6b46b3b51f8fe6aac2a64390c636242
Andrew Udvare <audvare@gmail.com>
2025-07-23 04:45:20 -0400

    main: QA fixes

--- mutt_oauth2/main.py
+++ mutt_oauth2/main.py
@@ -31,12 +31,12 @@ log = logging.getLogger(__name__)
 def get_handler(
         auth_code_callback: Callable[[str], None]) -> type[http.server.BaseHTTPRequestHandler]:
     class MyHandler(http.server.BaseHTTPRequestHandler):
-        def do_HEAD(self) -> None:  # noqa: N802
+        def do_HEAD(self) -> None:
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
 
-        def do_GET(self) -> None:  # noqa: N802
+        def do_GET(self) -> None:
             querydict = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
             if 'code' in querydict:  # pragma: no cover
                 auth_code_callback(querydict['code'][0])
